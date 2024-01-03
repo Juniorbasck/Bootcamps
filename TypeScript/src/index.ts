ff5ab80015db19339129b2242ac1b551381a966a
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-01-02 22:29:17 -0300

    enserindo bliblioteca

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,14 +1,2 @@
-type Hero ={
-    name: string,
-    vulgo: string
-};
-
-function PrintaPessoa(pessoa: Hero){
-    console.log(pessoa)
-}
-
-
-PrintaPessoa({
-    name: "junior", 
-    vulgo: "junin maldade pessoa", 
-});
+let dado: string = "mathues";
+console.log(dado);
